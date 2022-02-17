ba5c6e20c291f036380662b034c1cc84d71f86dd
ferhc2k <91056151+ferhc2k@users.noreply.github.com>
2022-02-16 21:56:05 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,70 +1,61 @@
 const content = document.querySelector(".content");
 const slider = document.querySelector(".slider");
-const sliderImage = Array.from(document.querySelectorAll(".slider-image"));
-const btnChevron = document.querySelectorAll(".btn-chevron");
+const images = [...document.querySelectorAll(".image")];
+const btnSlider = [...document.querySelectorAll(".btn-slider")];
 let i = 0;
 
-let reset = (container, clase) => {
-    container.forEach(item => item.classList.remove(clase));
-}
+const reset = (container, clase) => Array.from(container, item => item.classList.remove(clase));
 
-let createInfo = text => {
-    const sliderInfo = document.createElement("p");
-    sliderInfo.className = "slider-info";
-    sliderInfo.textContent = text;
-    content.appendChild(sliderInfo);
+const createDescription = text => {
+    const container = document.createElement("p");
+    container.className = "description";
+    container.textContent = text;
+    content.appendChild(container);
 };
 
-let createIndicators = () => {
+const createIndicators = () => {
     const container = document.createElement("div");
     container.className = "indicator";
-    content.appendChild(container)
-    sliderImage.forEach(image => {
-        let indicator = document.createElement("p");
-        indicator.textContent = sliderImage.indexOf(image) + 1;
+    images.map(image => {
+        const indicator = document.createElement("p");
+        indicator.textContent = images.indexOf(image) + 1;
+        indicator.addEventListener("click", () => {
+            i = images.indexOf(image);
+            moveImage();
+        })
         container.appendChild(indicator);
     })
+    content.appendChild(container);
 }
 
-let Image = (index) => {
+const setPosition = () => {
+    const { width } = images[i].getBoundingClientRect();
     const indicators = document.querySelectorAll('.indicator p');
-    const sliderInfo = document.querySelector('.slider-info');
-    sliderImage[index].classList.add('slider-image-active');
-    reset(indicators, 'indicator-active');
-
-    indicators[i].classList.add('indicator-active');
+    const description = document.querySelector('.description');
+    const hasElement = content.contains(document.querySelector(".description"));
     
-    if (content.hasElement(".slider-info")) return sliderInfo.textContent = sliderImage[index].dataset.info;
-    createInfo(sliderImage[index].dataset.info);   
+    reset(images, "image-active");
+    images[i].classList.add('image-active');
+    reset(indicators, 'indicator-active');
+    indicators[i].classList.add('indicator-active');
+    slider.style.transform = `translateX(-${width * i}px)`;
+
+    hasElement ? description.textContent = images[i].dataset.description : createDescription(images[i].dataset.description);   
 }
 
-let setPosition = (index) => {
-    let width = sliderImage[index].getBoundingClientRect().width;
-    slider.style.transform = `translateX(-${width * index}px)`;
-}
-
-let moveImage = () => {
-    if (i === sliderImage.length) {
-        i = 0; // Si el contador ya llego al ultimo item, lo manda al primer item.
-    } else if (i == -1) {
-        i = sliderImage.length - 1; // Si llego al primero lo manda hasta el ultimo.
-    }
-    reset(sliderImage, 'slider-image-active');
-    setPosition(i);
-    Image(i);
+const moveImage = () => {
+    if (i >= images.length) i = 0;
+    else if (i < 0) i = images.length - 1;
+    setPosition();
 };
 
-btnChevron.forEach(btn => {
+btnSlider.map(btn => {
     btn.addEventListener('click', () => {
-        if (btn.dataset.action == "right") {
-            i++;
-            return moveImage();
-        }
-        i--;
-        return moveImage();
+        btn.dataset.action === "right" ? i++ : i--;
+        moveImage();
     })
 })
 
 createIndicators();
-Image(i);
+setPosition();
 
